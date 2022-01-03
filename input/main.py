89f9b500288e5c70a4f2ea12915ba53e4bd88de0
joseSantorcuato <santorcuato76@gmail.com>
2022-01-03 13:48:04 -0300

    Topics of language, programming course. From variables to deep learning.

---
+++ input/main.py
@@ -0,0 +1,33 @@
+
+texto1 = 'INPUT'
+
+texto2 =  'Python en español'
+texto3 = 'José Luis Santorcuato Tapia'
+espacio = '   '
+an = '2021'
+print('Ingresa un número entero. Ej: 2')
+
+num = input()
+
+print(espacio)
+print(texto1)
+print(texto2)
+print(texto3)
+print(an)
+print(espacio)
+
+numint=int(num)
+
+# EJECUCIÓN
+
+a = 10
+b = 2
+c = 4
+print(a+b)
+print(a*numint) #INPUT
+print(type(num))
+print(a/c)
+print(a%c)
+print(int(a/c))
+print(pow(a,b))
+print(a ** b)
