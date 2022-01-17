a42929c7c0821d72abf7fbdfb6e8ceff6e7a49b6
joseSantorcuato <santorcuato76@gmail.com>
2022-01-17 12:57:42 -0300

    Se agregan funciones.

---
+++ funciones/main.py
@@ -0,0 +1,37 @@
+
+texto1 = 'FUNCIONES'
+
+texto2 =  'Python en español'
+texto3 = 'José Luis Santorcuato Tapia'
+espacio = '   '
+an = '2021'
+
+
+print(espacio)
+print(texto1)
+print(texto2)
+print(texto3)
+print(an)
+print(espacio)
+
+
+#EJECUCIÓN
+
+
+
+def func():
+    print("Python-LAB")
+
+# Driver code to call a function
+func()
+
+def funca(x,y):
+    print(x + y)
+# Driver code to call a function
+funca(10,20)
+
+def datos(nombre,apellido):
+    print(nombre, apellido)
+# Driver code to call a function
+datos(nombre= 'Mario', apellido='Mora Moreno')
+datos(nombre= 'Ximena', apellido='Jiménez Gimeno')
